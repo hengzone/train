6f0b11c331c8cd1de9edf54e4b6eae5d6078816b
Felix <hengzone@foxmail.com>
2018-06-18 20:56:10 +0800

    test

---
+++ pageManage/README.md
@@ -0,0 +1,21 @@
+# page-manage
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+研究 vue 生命周期，通过“套一层”的原则为 vue 实例增添 onEnter/onExit 两个额外的周期。
---
+++ pageManage/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+    <defaultPage name="helloWorld"> <helloWorld></helloWorld> </defaultPage>
+
+  </div>
+</template>
+
+<script>
+
+  import helloWorld from './components/helloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    helloWorld
+  },
+  mounted() {
+    PageManage.push('helloWorld');
+    setTimeout(() => {
+      PageManage.pop('helloWorld');
+    }, 5000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
