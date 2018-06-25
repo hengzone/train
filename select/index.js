3985519d74c09321c807cffb6c320698be2b334c
Felix <hengzone@foxmail.com>
2018-06-26 00:21:09 +0800

    模拟异步请求加载搜索结果

--- select/index.js
+++ select/index.js
@@ -2,6 +2,6 @@ import './src/style/client.css'
 
 import Select from './src/mySelect/Select'
 import arr1 from './src/data/test'
-import arr2 from './src/data/test2'
+import arr2 from './src/data/test2' // 模拟异步请求结果
 
 let select = new Select('mySelect', arr1, arr2);
